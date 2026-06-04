43bc99257253eff6a4b5908054828df586cf2562
omphy <54117994+omphy@users.noreply.github.com>
2026-06-04 11:36:58 +0200

    fix spawn speed

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,13 +5,18 @@ public class Spawner : MonoBehaviour
     public GameObject spawnObject;
     public Transform spawnTarget;
     GameObject createdObject;
+    float spawnTimer = 3f;
 
     // Update is called once per frame
     void Update()
     {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer > 0) return;
+
         if (!createdObject)
         {
             createdObject = Instantiate(spawnObject, spawnTarget.position, spawnTarget.rotation);
+            spawnTimer = 5f;
         }
         else
         {
