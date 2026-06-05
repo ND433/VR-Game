2317574e3b542e3272061236ec08d9f1882966ed
omphy <54117994+omphy@users.noreply.github.com>
2026-06-05 10:22:30 +0200

    Added feedback

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    public string tagName;
     public Score score;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Basketball") || other.CompareTag("Football"))
-        {
+        if (other.CompareTag(tagName))
             score.AddScore();
-        }
     }
 }
